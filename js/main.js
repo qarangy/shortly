cab14a007698ac97f42f8f9cee3b7e2b8a20d4f9
qarangy <qarangy@gmail.com>
2023-07-15 14:54:28 +0500

    add autofill styles

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const elUrlShortenerForm = document.querySelector('.js-url-shortener__form');
+const elUrlShortenerResults = document.querySelector('.url-shortener__results');
+
+elUrlShortenerForm.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+
+  elUrlShortenerResults.classList.add('url-shortener__results--open');
+});
